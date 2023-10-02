9471af22de09022aca74dfe15ce5543d23a92549
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-02 15:15:47 +0200

    these were debugging aids

--- run.js
+++
@@ -1,9 +0,0 @@
-const { JSON } = require('.');
-
-const json = '{ "array": [ 1 , 2] }';
-Promise.resolve(json)
-  .then((data) => JSON.parseAsync(data))
-  .then((document) => document.toObjectAsync())
-  .then((object) => {
-    console.log(object);
-  });
