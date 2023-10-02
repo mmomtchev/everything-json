076afc06a5425734fb789c3ba8cf4871cc0020f9
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-02 16:53:41 +0200

    add context to the becnhmarks

--- bench/JSON.parseAsync.bench.js
+++ bench/JSON.parseAsync.bench.js
@@ -12,27 +12,44 @@ const testJSON = fs.readFileSync(path.join(testDataPath, 'canada.json'), 'utf-8'
 module.exports = b.suite(
   'Asynchronous JSON parsing, accessing 1 element through each API\'s fastest interface',
 
-  b.add('built-in JSON.parse', async () => {
+  b.add('built-in JSON.parse (synchronous parsing)', async () => {
     const document = JSON.parse(testJSON);
     const data = document.features[0].geometry.coordinates[0][0];
     assert.isArray(data);
     assert.closeTo(data[0], -65.614, 1e-3);
     assert.closeTo(data[1], 43.42, 1e-3);
   }),
-  b.add('json-async', async () => {
+  b.add('json-async (100% background processing with zero latency)', async () => {
     const document = await JSONAsync.parseAsync(testJSON);
     const data = document.get().features.get()[0].get().geometry.get().coordinates.get()[0].get()[0].toObject();
     assert.isArray(data);
     assert.closeTo(data[0], -65.614, 1e-3);
     assert.closeTo(data[1], 43.42, 1e-3);
   }),
-  b.add('simdjson', () => {
+  b.add('simdjson (synchronous parsing)', () => {
     const document = simdjson.lazyParse(testJSON);
     const data = document.valueForKeyPath('features[0].geometry.coordinates[0][0]');
     assert.isArray(data);
     assert.closeTo(data[0], -65.614, 1e-3);
     assert.closeTo(data[1], 43.42, 1e-3);
   }),
+  b.add('yieldable-json (asynchronous parsing on the main thread by yielding)', async () => {
+    const document = await new Promise((resolve, reject) => {
+      try {
+        yieldable.parseAsync(testJSON, (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+    const data = document.features[0].geometry.coordinates[0][0];
+    assert.isArray(data);
+    assert.closeTo(data[0], -65.614, 1e-3);
+    assert.closeTo(data[1], 43.42, 1e-3);
+  }),
+
 
   b.cycle(),
   b.complete()
--- bench/JSON.toObjectAsync.bench.js
+++ bench/JSON.toObjectAsync.bench.js
@@ -12,7 +12,14 @@ const testJSON = fs.readFileSync(path.join(testDataPath, 'canada.json'), 'utf-8'
 module.exports = b.suite(
   'Asynchronous JSON parsing, converting the data to a native JS object',
 
-  b.add('yieldable-json', async () => {
+  b.add('built-in JSON.parse (synchronous parsing)', async () => {
+    const document = JSON.parse(testJSON);
+    const data = document.features[0].geometry.coordinates[0][0];
+    assert.isArray(data);
+    assert.closeTo(data[0], -65.614, 1e-3);
+    assert.closeTo(data[1], 43.42, 1e-3);
+  }),
+  b.add('yieldable-json (yieldable parsing on the main thread)', async () => {
     const document = await new Promise((resolve, reject) => {
       try {
         yieldable.parseAsync(testJSON, (err, result) => {
@@ -28,7 +35,7 @@ module.exports = b.suite(
     assert.closeTo(data[0], -65.614, 1e-3);
     assert.closeTo(data[1], 43.42, 1e-3);
   }),
-  b.add('json-async', async () => {
+  b.add('json-async (background parsing with yieldable object construction)', async () => {
     const document = await (await JSONAsync.parseAsync(testJSON)).toObjectAsync();
     const data = document.features[0].geometry.coordinates[0][0];
     assert.isArray(data);
