93f874b4387b761834bf5422042045dc7019c1a4
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-02 12:29:51 +0200

    iterative tree traversal

--- bench/JSON.toObjectAsync.bench.js
+++ bench/JSON.toObjectAsync.bench.js
@@ -3,7 +3,7 @@ const path = require('path');
 const b = require('benny');
 const { assert } = require('chai');
 
-const simdjson = require('simdjson');
+const yieldable = require('yieldable-json');
 const JSONAsync = require('..').JSON;
 
 const testDataPath = path.resolve(__dirname, '..', 'test', 'data');
@@ -12,8 +12,17 @@ const testJSON = fs.readFileSync(path.join(testDataPath, 'canada.json'), 'utf-8'
 module.exports = b.suite(
   'Asynchronous JSON parsing, converting the data to a native JS object',
 
-  b.add('built-in JSON.parse', async () => {
-    const document = JSON.parse(testJSON);
+  b.add('yieldable-json', async () => {
+    const document = await new Promise((resolve, reject) => {
+      try {
+        yieldable.parseAsync(testJSON, (err, result) => {
+          if (err) reject (err);
+          resolve(result);
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
     const data = document.features[0].geometry.coordinates[0][0];
     assert.isArray(data);
     assert.closeTo(data[0], -65.614, 1e-3);
@@ -21,13 +30,6 @@ module.exports = b.suite(
   }),
   b.add('json-async', async () => {
     const document = await (await JSONAsync.parseAsync(testJSON)).toObjectAsync();
-    const data = document.features[0].geometry.coordinates[0][0];
-    assert.isArray(data);
-    assert.closeTo(data[0], -65.614, 1e-3);
-    assert.closeTo(data[1], 43.42, 1e-3);
-  }),
-  b.add('simdjson', () => {
-    const document = simdjson.parse(testJSON);
     const data = document.features[0].geometry.coordinates[0][0];
     assert.isArray(data);
     assert.closeTo(data[0], -65.614, 1e-3);
