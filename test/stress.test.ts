53c1f8688bee0bf0807dc3ad204c2e8304928d35
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-03 14:28:37 +0200

    cover all test files including parallel tests

--- test/stress.test.ts
+++ test/stress.test.ts
@@ -4,29 +4,43 @@ import { assert } from 'chai';
 
 import { JSON as JSONAsync } from 'everything-json';
 
-describe('stress', () => {
-  const text = fs.readFileSync(path.resolve(__dirname, 'data', 'canada.json'), 'utf8');
-  const expected = JSON.parse(text);
+describe('stress', function () {
+  this.timeout(20000);
+  const dir = path.resolve(__dirname, 'data');
+  const files = fs.readdirSync(dir);
+  const texts = files.filter((file) => file.match(/json$/)).map((file) => fs.readFileSync(path.resolve(dir, file), 'utf-8'));
+  const expected = texts.map((text) => JSON.parse(text));
 
-  it('ensure that the GC does not free the string', () => {
-    let document;
-    for (let i = 0; i < 50; i++)
-      setImmediate(() => {
-        document = JSONAsync.parse(text);
+  it('ensure that the GC does not free the string', function (done) {
+    let completed = 0;
+    for (let i = 0; i < texts.length * 2; i++) {
+      const idx = i % texts.length;
+      setImmediate(async () => {
+        try {
+          const document = JSONAsync.parse(texts[idx]).toObject();
+          assert.deepEqual(document, expected[idx]);
+          completed++;
+          if (completed === texts.length * 2)
+            done();
+        } catch (e) {
+          done(new Error(`${files[idx]} : ${e}`));
+        }
       });
+    }
   });
 
-  it('ensure that the deferred is not destroyed too early', function(done) {
-    this.timeout(20000);
+  it('ensure that the deferred is not destroyed too early', function (done) {
     (async function () {
-      for (let i = 0; i < 25; i++) {
-        const document = await (await JSONAsync.parseAsync(text)).toObjectAsync() as Record<string, any>;
-        const data = document.features[0].geometry.coordinates[0][0];
-        assert.isArray(data);
-        assert.closeTo(data[0], -65.614, 1e-3);
-        assert.closeTo(data[1], 43.42, 1e-3);
-        assert.deepEqual(document, expected);
+      const q: Promise<void>[] = [];
+      for (let i = 0; i < texts.length * 2; i++) {
+        const idx = i % texts.length;
+        q.push(JSONAsync.parseAsync(texts[idx])
+          .then((json) => json.toObjectAsync())
+          .then((document) => {
+            assert.deepEqual(document, expected[idx]);
+          }));
       }
+      await Promise.all(q);
     })()
       .then(done)
       .catch(done);
