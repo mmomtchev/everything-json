8f3e18aa0a1819d4a8d7a32d744a7d446617612f
Momtchil Momtchev <momtchil@momtchev.com>
2023-10-04 18:43:37 +0200

    add a yielding latency unit test

---
+++ test/latency.test.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as zlib from 'zlib';
+import { assert } from 'chai';
+import type { FeatureCollection } from 'geojson';
+
+import { JSON as JSONAsync } from 'everything-json';
+
+const jsonText = zlib.unzipSync(fs.readFileSync(path.resolve(__dirname, 'data', 'sf_citylots.json.gz')));
+
+it('toObjectAsync() yields the CPU', function (done) {
+  this.timeout(30000);
+
+  // We start counting the elapsed time
+  // and we launch a regular tick every 10ms
+  const start = Date.now();
+  let ticks = 0;
+  const timer = setInterval(() => void ticks++, 10);
+  JSONAsync.latency = 1;
+
+  JSONAsync.parseAsync<FeatureCollection>(jsonText)
+    .then((jsonBinary) => {
+      const elapsed = Date.now() - start;
+      // Manpower losses of up to 25% do not have to be reported to higher command
+      assert.isAtLeast(ticks, elapsed / 10 * 0.75);
+
+      return jsonBinary.toObjectAsync();
+    })
+    .then((geojson) => {
+      assert.strictEqual(geojson.type, 'FeatureCollection');
+      assert.isArray(geojson.features);
+
+      const elapsed = Date.now() - start;
+      assert.isAtLeast(ticks, elapsed / 10 * 0.75);
+    })
+    .catch(done)
+    .then(() => {
+      JSONAsync.latency = 5;
+      clearInterval(timer);
+      done();
+    });
+});
